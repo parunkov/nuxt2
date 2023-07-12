f3b07c83ca934db36e3a1eae82699e2d38771ad0
parunkov <parunkov@gmail.com>
2023-07-12 16:56:10 +0500

    bootstrap add

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,26 @@
-<div>!!!</div>
+<template>
+    <div class="title">
+      <h2>Login</h2>
+    </div>
+    <div class="container form">
+      <label for="uname"><b>Username</b></label>
+      <input
+        type="text"
+        class="input"
+        placeholder="Enter Username"
+        name="uname"
+        required
+      />
+
+      <label for="psw"><b>Password</b></label>
+      <input
+        type="password"
+        class="input"
+        placeholder="Enter Password"
+        name="psw"
+        required
+      />
+
+      <button class="button">Login1</button>
+    </div>
+</template>
