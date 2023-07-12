0786513f248a333759f40a8cb3f59bfecd8926c1
parunkov <parunkov@gmail.com>
2023-07-12 17:19:52 +0500

    form styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="container">
-      <div class="title">
+      <div class="title mt-3">
         <h2>Авторизация</h2>
       </div>
       <form class="form">
-        <label for="uname" class="form-label"><b>Логин</b></label>
+        <label for="login" class="form-label"><b>Логин</b></label>
         <input
           type="text"
           class="form-control mb-3"
           placeholder="Логин"
-          name="uname"
+          name="login"
           required
         />
-        <label for="psw" class="form-label"><b>Пароль</b></label>
+        <label for="password" class="form-label"><b>Пароль</b></label>
         <input
           type="password"
           class="form-control mb-3"
           placeholder="Пароль"
-          name="psw"
+          name="password"
           required
         />
-        <button class="btn btn-primary mb-3">Войти</button>
+        <button type="submit" class="btn btn-primary mb-3">Войти</button>
       </form>
     </div>
 </template>
 <style scoped>
   .form {
-    width: 400px;
+    width: 300px;
   }
 </style>
