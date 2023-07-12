dbf1519dd784ef2149f9571a1ab3f38e79810de5
parunkov <parunkov@gmail.com>
2023-07-12 17:16:02 +0500

    form creating

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,31 @@
 <template>
-    <div class="title">
-      <h2>Login</h2>
-    </div>
-    <div class="container form">
-      <label for="uname"><b>Username</b></label>
-      <input
-        type="text"
-        class="input"
-        placeholder="Enter Username"
-        name="uname"
-        required
-      />
-
-      <label for="psw"><b>Password</b></label>
-      <input
-        type="password"
-        class="input"
-        placeholder="Enter Password"
-        name="psw"
-        required
-      />
-
-      <button class="button">Login1</button>
+    <div class="container">
+      <div class="title">
+        <h2>Авторизация</h2>
+      </div>
+      <form class="form">
+        <label for="uname" class="form-label"><b>Логин</b></label>
+        <input
+          type="text"
+          class="form-control mb-3"
+          placeholder="Логин"
+          name="uname"
+          required
+        />
+        <label for="psw" class="form-label"><b>Пароль</b></label>
+        <input
+          type="password"
+          class="form-control mb-3"
+          placeholder="Пароль"
+          name="psw"
+          required
+        />
+        <button class="btn btn-primary mb-3">Войти</button>
+      </form>
     </div>
 </template>
+<style scoped>
+  .form {
+    width: 400px;
+  }
+</style>
